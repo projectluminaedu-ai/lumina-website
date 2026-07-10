cf58637ab557f895780f567322bcf32abdfab0df
afangbc <austin.fang.001@gmail.com>
2026-07-10 18:03:19 -0500

    Updated website details

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // CHANGE THE GOOGLE FORM LINK HERE
-const JOIN_FORM_URL = "https://forms.google.com/your-form-link";
+const JOIN_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfR89B9IfgpTcB3IfKXrjuN4BL5PTcP1BQKvgogZ4wt2McWiw/viewform?usp=publish-editor";
